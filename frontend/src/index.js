f6a4a1788dd50368977c9139bbd7302f97bae0a4
Argonaut790 <tung23966373@gmail.com>
2023-03-12 15:42:26 +0800

    fixed html header, so that we don't have to link but use module of bootstrap5, react and babel.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";

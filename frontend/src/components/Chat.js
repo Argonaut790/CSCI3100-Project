34e6cf618cf368e1803a3c0158c090fc6eda7bc2
Argonaut790 <tung23966373@gmail.com>
2023-03-13 16:48:40 +0800

    init chat and profile js file, so we can divide work there.

---
+++ frontend/src/components/Chat.js
@@ -0,0 +1,14 @@
+// import { Component } from "react";
+// import axios from "axios";
+
+const Chat = () => {
+  return (
+    <>
+      <div className="col-md" id="content">
+        <div className="container-fluid p-0 h-100">Chat</div>
+      </div>
+    </>
+  );
+};
+
+export default Chat;

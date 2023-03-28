1d469c71fd8bbc122b628fcea510518af7170466
Nico Cheung <nico_ckt@yahoo.com.hk>
2023-03-29 01:08:52 +0800

    set up follower & following list frontend

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,60 +1,11 @@
 import ImportAll from "./ImportAll";
 
 //function needs to be Capital Letter in the first
-const Search = ({ loggedIn: { loggedIn } }) => {
-  return (
-    <>
-      <Searchbar />
-      {loggedIn && (
-        <ul className="list-group" id="following-div">
-          <li className="list-group-item" id="following-label">
-            Following
-          </li>
-          <li className="list-group-item">
-            <UserID />
-          </li>
-          <li className="list-group-item">
-            <UserID />
-          </li>
-          <li className="list-group-item">
-            <UserID />
-          </li>
-          <li className="list-group-item">
-            <UserID />
-          </li>
-          <li className="list-group-item">
-            <UserID />
-          </li>
-          <li className="list-group-item">
-            <UserID />
-          </li>
-        </ul>
-      )}
-    </>
+const Search = () => {
+  const images = ImportAll(
+    require.context("../images", false, /\.(png|jpe?g|svg)$/)
   );
-};
 
-const images = ImportAll(
-  require.context("../images", false, /\.(png|jpe?g|svg)$/)
-);
-
-const UserID = () => {
-  return (
-    <div className="post-user-info">
-      <img
-        src={images["user_avatar.jpg"]}
-        className="float-start post-user-avatar"
-        alt="user-avatar"
-      />
-      <div className="d-flex align-items-md-center h-100 m-0 post-user-id">
-        <div className="fw-bold">UserName</div>
-        <div>#UserID</div>
-      </div>
-    </div>
-  );
-};
-
-const Searchbar = () => {
   return (
     <div>
       <div className="form-floating" id="search-engine-div">

afbd54e782acfe8ab2da1a7cbe7034a48c050a83
Nico Cheung <nico_ckt@yahoo.com.hk>
2023-04-14 01:08:35 +0800

    follow lists for user profiles

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -4,6 +4,7 @@
 import UserProfileContent from "./UserProfileContent";
 import Search from "./Search";
 import { useSearchParams, useNavigate } from "react-router-dom";
+import Follow from "./Follow";
 
 //function needs to be Capital Letter in the first
 const UserProfile = ({ userId, maskBackgroundRef }) => {
@@ -24,8 +25,9 @@ const UserProfile = ({ userId, maskBackgroundRef }) => {
         />
       </div>
       <div className="col-lg-3 vh-100" id="explore">
-        <div className="container-fluid p-0 mt-4" id="rhs">
+        <div className="container-fluid p-0 d-grid gap-3 mt-4" id="rhs">
           <Search userId={userId} />
+          <Follow userId={userId} />
         </div>
       </div>
     </>
